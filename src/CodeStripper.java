a22e860db45473e5f14b995c994e354a3c19cf6d
Ali <ali.cooom@gmail.com>
2016-02-09 22:30:24 -0700

    Implementing the stripping method..works but still doesn't strip comments correctly.

--- src/CodeStripper.java
+++ src/CodeStripper.java
@@ -1,89 +1,157 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.Scanner;
 
 public class CodeStripper {
 
     private File file;
     BufferedReader buffer;
-    private String outputFile;
+    private String outputFileName;
+    private String finalString;
     private PrintWriter pw;
 
 
-    public CodeStripper(File file, BufferedReader br){
-        this.file=file;
-        buffer=br;
-        setOutputFile();
+    public CodeStripper(File file, BufferedReader br) {
+        this.file = file;
+        this.buffer = br;
+        setOutputFileName();
 
         try {
-            File outputfile = new File(outputFile);
+            File outputfile = new File(outputFileName);
             pw = new PrintWriter(outputfile);
-            LinesReader();
+            LinesTrimmer();
+            pw.print(finalString);
+            pw.close();
 
-        }catch (Exception e){
+
+        } catch (Exception e) {
             System.out.println("PrintWriter issue");
+            e.printStackTrace();
         }
+
     }
 
 
-
-    public void LinesReader() throws IOException{
-
+    // ##########################################################
+    // This method will trim any blank line and a single line comment that has no spaces (a common case)
+    public void LinesTrimmer() throws IOException {
 
         String line;
-        String strippedLine;
+        String trimmedString;
+        StringBuilder blder = new StringBuilder();
+
+
         while ((line = buffer.readLine()) != null) {
 
-           // if (line.equals("\n")){System.out.println("a new line is removed")}
-            //else if (line.startsWith("\t") &&line.)
-            //List<Character> list = new ArrayList<>(line.toCharArray());
+            if (line.startsWith("//")) {
+                continue;
+            }
+
+            if (line.trim().length() == 0) {
+                continue;
+            }
+            blder.append(line);
+        }//end of while
+
+        trimmedString = blder.toString();
+
+        //System.out.println("trimmed      " +trimmedString);
+        //ArrayList<Character> list = new ArrayList<Character>();
+
+        commentsStripper(trimmedString);
+    }
+
+    // ##########################################################
+    // This enum will be used to change the flag with each iteration with each character
+    // String flag is inessential for cases such as when having a comment character within a string
+    enum Flag {
+        TEXT_CODE, B_L_COMMENT_BEGINNING, COMMENT_ENDING, LINE_COMMENT, BLOCK_COMMENT, STRING
+    }
 
 
+    // ##########################################################
+    public void
+    commentsStripper(String str) {
+
+        Flag flag = Flag.TEXT_CODE;
+
+        StringBuilder strBldr = new StringBuilder();
+        char currentChar = ' ';
+        char previousChar = ' ';
 
 
-            //pw.println(line);
-            //pw.println("ops");
+        for (int i = 0; i < str.length(); i++) {
+            currentChar = str.charAt(i);
+            switch (flag) {
+                case TEXT_CODE:
+                    if (currentChar == '/')
+                        flag = Flag.B_L_COMMENT_BEGINNING;
+                    else if (currentChar == '"') {
+                        strBldr.append(currentChar);
+                        flag = Flag.STRING;
+                    } else {
+                        strBldr.append(currentChar);
+                    }
+                    break;
+                case B_L_COMMENT_BEGINNING:
+                    if (currentChar == '/') {
+                        flag = Flag.LINE_COMMENT;
+                    } else if (currentChar == '*') {
+                        flag = Flag.BLOCK_COMMENT;
+                    } else {
+                        flag = Flag.TEXT_CODE;
+                        strBldr.append(previousChar + currentChar);
+                    }
+                    break;
+                case COMMENT_ENDING:
+                    if (currentChar != '*')
+                        flag = Flag.BLOCK_COMMENT;
+                    else if (currentChar == '/')
+                        flag = Flag.TEXT_CODE;
+                case STRING:
+                    if (currentChar == '"' && previousChar != '\\')
+                        flag = Flag.TEXT_CODE;
+                    strBldr.append(currentChar);
+                    break;
+                case BLOCK_COMMENT:
+                    if (currentChar == '*')
+                        flag = Flag.COMMENT_ENDING;
+                    break;
+                case LINE_COMMENT:
+                    if (currentChar == '\n') {
+                        flag = Flag.TEXT_CODE;
+                        strBldr.append(currentChar);
+                    }
+                    break;
+            }//end of switch
+            previousChar = currentChar;
+        }//end of for
 
+        String strippedString = strBldr.toString();
+        StringBuilder moreStrippedString = new StringBuilder();
+        Scanner scanner = new Scanner(strippedString);
+
+        // this while loop is to trim blank lines generated from trimming comments
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.trim().length() == 0) {
+                continue;
+            }
+            moreStrippedString.append(line);
         }
-        pw.close();
 
-    }
-
-    public String blankLineStripper(List<Character> list){
-
-//        Iterator<Character> itr = list.iterator();
-//        while (itr.hasNext()){
-//            if()
-
-
-        }
-
-    }
-
-    public String lineCommentStripper(){
-
-    }
-
-    public String blockCommentStripper(){
+        finalString = moreStrippedString.toString();
 
 
     }
+    // ##########################################################
 
-
-
-
-
-    public void setOutputFile(){
+    public void setOutputFileName() {
 
         String inputFileName = file.getName();
-        System.out.println("input file name ==>"+inputFileName);
         StringBuilder stbr = new StringBuilder(inputFileName);
-        stbr.insert(inputFileName.indexOf("."),"out");
-        System.out.println("output file naem ###>"+stbr.toString());
-        outputFile = stbr.toString();
+        stbr.insert(inputFileName.indexOf("."), "-out");
+        outputFileName = stbr.toString();
     }
-
 
 
 }
--- src/InputFilesManager.java
+++ src/InputFilesManager.java
@@ -1,57 +1,66 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.io.IOException;
 
 public class InputFilesManager {
 
     private final int NUMBER_OF_FIlES = 13;
-    private String []letters= {"a", "b", "c", "d"};
+    private String[] letters = {"a", "b", "c", "d"};
 
 
     public InputFilesManager() {
-        generateFileNames();
+        generateInputFileNames();
     }
 
-    public void generateFileNames() {
+    public void generateInputFileNames() {
 
-        //for(int i = 1;i<=NUMBER_OF_FIlES;i++)
-       // {
-//            StringBuilder stbr = new StringBuilder();
-//            int j=0;
-//            String theNum = String.valueOf(i);
-//            stbr.append("../src/striptest-");
-//            stbr.append(theNum);
-//
-//            if (i > 9 && i<14) {
-//                stbr.append(letters[j]);
-//                j++;
-//            }
-//            stbr.append(".txt");
-//            System.out.println(stbr);
-        String stbr = "striptest-3.txt";
-            fileOpener(stbr/*.toString()*/);
-      //  }
+        int NUMBER_OF_MY_FILES = 13;
+        String[] letters = {"a", "b", "c", "d"};
+
+        for (int i = 1; i <= NUMBER_OF_MY_FILES; i++) {
+            StringBuilder stbr = new StringBuilder();
+            String theNum = String.valueOf(i);
+            stbr.append("striptest-");
+            stbr.append(theNum);
+
+            if (i == 10) {
+                stbr.append("a");
+            }
+            if (i == 11) {
+                stbr.append("a");
+            }
+            if (i == 12) {
+                stbr.append("a");
+            }
+            if (i == 13) {
+                stbr.append("a");
+            }
+
+            stbr.append(".txt");
+            System.out.println(stbr.toString());
+            inputFilesGenerator(stbr.toString());
+
+        }
+
+
     }
 
 
-    public void fileOpener(String str){
+    public void inputFilesGenerator(String str) {
 
         try {
             File file = new File(str);
             BufferedReader br = new BufferedReader(new FileReader(file));
-             CodeStripper stripper = new CodeStripper(file,br);
+            CodeStripper stripper = new CodeStripper(file, br);
 
-        }catch (IOException e){
+        } catch (Exception e) {
 
-            System.out.println("Not Found #################################################");
+            System.out.println("Input file issue #################################################");
             e.printStackTrace();
         }
 
 
-
     }
-
 
 
 }
